2b62f479a2e9299943079de76ea9f3c36418cfd4
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2024-01-15 15:33:18 -0300

    feat: create meals

---
+++ src/routes/meals.ts
@@ -0,0 +1,39 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { checkSessionIdExists } from '../middleware/check-session-id-exists'
+import { knex } from '../database'
+import { randomUUID } from 'crypto'
+
+export async function mealsRoutes(app: FastifyInstance) {
+  app.addHook('preHandler', async (request) => {
+    console.log(`[${request.method}] ${request.url}`)
+  })
+
+  app.post(
+    '/',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const createMealsBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+        date: z.coerce.date(),
+      })
+
+      const { name, description, isOnDiet, date } = createMealsBodySchema.parse(
+        request.body,
+      )
+
+      await knex('meals').insert({
+        id: randomUUID(),
+        name,
+        description,
+        is_on_diet: isOnDiet,
+        date: date.getTime(),
+        user_id: request.user?.id,
+      })
+
+      return reply.status(201).send()
+    },
+  )
+}
